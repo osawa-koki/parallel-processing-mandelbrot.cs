cf5c3bf06e94b1443f95dbaa26cd4e6b78470954
osawa-koki <koki@osawa.dev>
2024-01-01 16:17:21 +0900

    プロジェクトの初期化。

---
+++ Program/Math.cs
@@ -0,0 +1,15 @@
+namespace simple_project_cs
+{
+    public class Math
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# parallel-processing-mandelbrot.cs
+
+🧧🧧🧧 C#で並列処理でマンデルブロ集合を描画してみる！  
